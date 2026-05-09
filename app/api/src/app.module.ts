3e8c396933481c27884ffe6929aa5db0dd32e52c
Verttb <alyssonoliveirai456@gmail.com>
2026-05-09 14:22:11 -0300

    feat: Add User and Review module

--- app/api/src/app.module.ts
+++ app/api/src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './resources/users/users.module';
+import { ReviewsModule } from './resources/reviews/reviews.module';
 @Module({
   imports: [ConfigModule.forRoot({
     envFilePath: '.env',
     isGlobal: true
-  }), PrismaModule],
+  }), PrismaModule, UsersModule, ReviewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
