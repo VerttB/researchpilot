3a9fe1ec61bc272e7b42d0ba5db4b4b9555f0b6c
Verttb <alyssonoliveirai456@gmail.com>
2026-05-12 22:00:45 -0300

    chore: Change swagger title and tag

--- app/api/src/main.ts
+++ app/api/src/main.ts
@@ -11,10 +11,10 @@ async function bootstrap() {
   app.use(cookieParser());
 
    const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Research Pilot')
+    .setDescription('The Research Pilot API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('ResearchPilot')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
