c154451b48604a63455558483ae8761cddbc0cde
Verttb <alyssonoliveirai456@gmail.com>
2026-05-12 21:40:26 -0300

    refactor: Moving jwtModule to App module Now registers jwt with async to Getting secret using configService

--- app/api/src/app.module.ts
+++ app/api/src/app.module.ts
@@ -1,16 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './resources/users/users.module';
 import { ReviewsModule } from './resources/reviews/reviews.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './helpers/guards/auth/auth.guard';
+import { AuthModule } from './resources/auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
 @Module({
   imports: [ConfigModule.forRoot({
     envFilePath: '.env',
     isGlobal: true
-  }), PrismaModule, UsersModule, ReviewsModule],
+  }), PrismaModule, {
+    module: PrismaModule,
+    global: true
+  }, JwtModule.registerAsync({
+    inject: [ConfigService],
+    global: true,
+    useFactory: (config: ConfigService) => ({
+      secret: config.get("JWT_SECRET"),
+      signOptions: { expiresIn: '1h' },
+    })
+   
+  }),
+  
+  UsersModule, ReviewsModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_GUARD,
+    useClass: AuthGuard
+  }],
 })
 export class AppModule {}
--- app/api/src/resources/auth/auth.module.ts
+++ app/api/src/resources/auth/auth.module.ts
@@ -2,15 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
-import { JwtModule } from '@nestjs/jwt';
+
 @Module({
 
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [UsersModule, JwtModule.register({
-    secret: process.env.JWT_SECRET,
-    global: true,
-    signOptions: { expiresIn: '1h' },
-  })],
+  imports: [UsersModule],
 })
 export class AuthModule {}
