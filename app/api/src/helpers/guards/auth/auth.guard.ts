3c872ec50061fccb4f66a54022f39e93bb5b87d3
Verttb <alyssonoliveirai456@gmail.com>
2026-05-12 21:38:33 -0300

    chore: Moving guard to its own folder

---
+++ app/api/src/helpers/guards/auth/auth.guard.ts
@@ -0,0 +1,51 @@
+
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import type { Request } from 'express';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '../../decorators/skipauth.decorator';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService, private reflector: Reflector) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      
+      const payload = await this.jwtService.verifyAsync(token);
+     
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const token = request.cookies['access_token'] || request.headers['authorization']?.split(' ')[1];
+    return token
+  }
+
+}
+
+
+
+
