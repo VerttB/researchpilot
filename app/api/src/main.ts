5de1f0cf0f1c06df419c63960a7dc8c435a1808d
Verttb <alyssonoliveirai456@gmail.com>
2026-05-19 19:03:08 -0300

    refactor: moves logic for config file

--- app/api/src/main.ts
+++ app/api/src/main.ts
@@ -1,25 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import cookieParser from 'cookie-parser';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { HttpExceptionFilter } from './helpers/filters/exceptions.filter';
+import { setDefaultConfig } from './config/config.setter';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
-  app.use(cookieParser());
-  app.useGlobalFilters(new HttpExceptionFilter())
-   const config = new DocumentBuilder()
-    .setTitle('Research Pilot')
-    .setDescription('The Research Pilot API description')
-    .setVersion('1.0')
-    .addTag('ResearchPilot')
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
-
+  setDefaultConfig(app);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
